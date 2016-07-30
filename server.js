9acc8d361eebc40591086ae7ed54d682c861ec45
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-07-29 22:32:46 -0500

    added node and express

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+//var router = require('express').Router();
+var path = require('path');
+
+app.get('/', function(request, response){
+  response.sendFile(path.join(__dirname, '/index.html'));
+});
+
+var server = app.listen(process.env.PORT || 3000, function() {
+  var port =server.address().port;
+console.log('Listening on port', port);
+});
